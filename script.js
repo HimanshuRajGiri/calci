a7a3fdebef72ec28803ff9b1d92b99a657011de1
Himanshu Raj Giri <hrgiri2004@gmail.com>
2025-09-13 16:11:42 +0530

    add something

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const input = document.querySelector('.input');
 const buttons = document.querySelectorAll('#button');
+const clear = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
